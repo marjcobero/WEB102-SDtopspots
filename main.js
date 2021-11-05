60958fee400f4444baa8fe2a8304cfab2e510d10
Marjorie <cobero.marjorie@gmail.com>
2021-11-05 12:09:30 -0700

    task completed

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-// write your code here
-
+	// write your code here
+    $.getJSON("data.json", function(data) {
+        for (i = 0; i < data.length; i++) {
+        $("#info").append(
+            "<tr>" +
+            "<td>" + data[i]["name"] + "</td>" +
+            "<td>" + data[i]["description"] + "</td>" +
+            "<td>" + "<button href=\"" + "https://www.google.com/maps?q=33.09745,-116.99572" + data[i]["location"][0] + "," + data[i]["location"][1] + "\"" + " class=\"loc-button\">Open in Google Maps</button>" + "</td>" +
+            "</tr>"
+        );
+        }
+    });
 });
